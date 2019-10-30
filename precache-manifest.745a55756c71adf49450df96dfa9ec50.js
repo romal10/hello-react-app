2aa4269b815a58af4dd17fe993a367af9db2bb61
romal <mohey.romal@gmail.com>
2019-10-30 11:27:22 +0100

    Updates

---
+++ precache-manifest.745a55756c71adf49450df96dfa9ec50.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a98ed97b337e9e9e29c02e3cbb4d3bbd",
+    "url": "/hello-react-app/index.html"
+  },
+  {
+    "revision": "4f0985a18755eca6e449",
+    "url": "/hello-react-app/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "5bd14360b95d228df49d",
+    "url": "/hello-react-app/static/js/2.04ae8c2c.chunk.js"
+  },
+  {
+    "revision": "4f0985a18755eca6e449",
+    "url": "/hello-react-app/static/js/main.c258f758.chunk.js"
+  },
+  {
+    "revision": "63180964b893046eac22",
+    "url": "/hello-react-app/static/js/runtime-main.9c74c70a.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/hello-react-app/static/media/logo.25bf045c.svg"
+  }
+]);
